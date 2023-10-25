bce7db9f4f1751ebd0e61bdcbc94d875bfbd049b
Faith-M <makinafaith0@gmail.com>
2023-10-24 19:22:21 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,17 +2,15 @@
 #include <stdio.h>
 
 /**
- * _strspn - entry point
+ * _strspn - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int n = 0;
 int r;
-
 while (*s)
 {
 for (r = 0; accept[r]; r++)
@@ -28,4 +26,4 @@ return (n);
 s++;
 }
 return (n);
-}				
+}
