70fb92475fd2743c53753af05d79df0b7d21c295
Faith Makina <makinafaith0@gmail.com>
2024-07-06 13:10:43 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
