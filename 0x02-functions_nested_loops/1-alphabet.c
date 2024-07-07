be825858d7a563be443d42252def133011e6c41e
Faith Makina <makinafaith0@gmail.com>
2024-07-06 18:52:27 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * main - Prints the alphabet, in lowercase
- * Return: 0 (Success)
+ * print_alphabet - Prints the alphabet, in lowercase followed by new line
+ * No return because header function is void
  */
-
 void print_alphabet(void)
 {
 	char letter;
