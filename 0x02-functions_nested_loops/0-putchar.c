bae0eedc139b7a25f3245876a7609a1969c04578
Faith Makina <makinafaith0@gmail.com>
2024-07-06 17:08:14 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints _putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	/*char _putchar;*/
+
+
+	/*putchar(_putchar);*/
+	printf("_putchar");
+	putchar('\n');
+
+	return (0);
+}
