52ae136b88b580ffe27c6265939dca9668782bac
Faith Makina <makinafaith0@gmail.com>
2024-07-07 14:51:35 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
+ * No return
+ */
+
+void print_alphabet_x10(void)
+{
+	int times;
+	char letter;
+
+	for (times = 0; times == 10; times++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+}
