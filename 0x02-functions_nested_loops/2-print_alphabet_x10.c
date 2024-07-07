fa4a898e044650be7a9b8044b0408c1d09f60e7a
Faith Makina <makinafaith0@gmail.com>
2024-07-07 14:53:49 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int times;
 	char letter;
 
-	for (times = 0; times == 10; times++)
+	for (times = 0; times < 10; times++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
