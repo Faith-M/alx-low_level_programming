54eb17110503e1ece35977583111d19dfa576229
Faith Makina <makinafaith0@gmail.com>
2024-07-08 06:33:28 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * mul - Multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
