bf7201c9695989064bf3237d60ca1d0ab7843300
Faith Makina <makinafaith0@gmail.com>
2024-07-08 01:11:08 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers, separated by comma and space
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, next_fib;
+	int count;
+
+	printf("%lu, %lu", fib1, fib2);  /*Print the first two Fibonacci numbers*/
+
+	for (count = 3; count <= 98; ++count)
+	{
+		next_fib = fib1 + fib2;
+		printf(", %lu", next_fib);
+
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+
+	printf("\n");
+
+	return (0);
+}
