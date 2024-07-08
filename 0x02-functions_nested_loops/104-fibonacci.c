d58bf010f856b075bc8234cfb47bfbaf5982033a
Faith Makina <makinafaith0@gmail.com>
2024-07-08 01:35:53 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,15 @@
 
 int main(void)
 {
-	unsigned long long fib1 = 1, fib2 = 2;
-	unsigned long long next_fib;
+	unsigned long fib1 = 1, fib2 = 2, next_fib;
 
-	printf("%llu, %llu", fib1, fib2);
+	printf("%lu, %lu", fib1, fib2);
 
 	for (int count = 3; count <= 98; ++count)
 	{
 		next_fib = fib1 + fib2;
 
-		printf(", %llu", next_fib);
+		printf(", %lu", next_fib);
 
 		fib1 = fib2;
 		fib2 = next_fib;
