1d00ee0c3fc8c4029c5351f383fee88f5a681658
Faith Makina <makinafaith0@gmail.com>
2024-07-08 00:08:52 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * add - Adds two integers and returns the result
+ * @a: First integer
+ * @b: Second integer
+ * Return: Result of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
