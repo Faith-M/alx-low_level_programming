58a003248161f3a02f09dc6470668bd41b72b572
Faith Makina <makinafaith0@gmail.com>
2024-07-08 15:05:56 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
 #include <math.h>
 
 /**
- * reset_to_98 - Takes a pointer to an int as parameter and updates the value it points to
+ * reset_to_98 - Takes a pointer to an int as parameter and updates the value
  * @n: The integer
  * Return: Void
  */
