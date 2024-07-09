a6c29de108767ce62be73f64b865441e4ff51780
Faith Makina <makinafaith0@gmail.com>
2024-07-09 00:40:19 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, separated by comma and space
- * Return: 0 (Success)
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, next_fib;
 	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_high1, fib1_low1, fib2_high1, fib2_low1;
+	unsigned long high1, low1;
 
-	printf("%lu, %lu", fib1, fib2);
-
-	for (count = 3; count <= 98; ++count)
+	for (count = 0; count < 92; count++)
 	{
-		next_fib = fib1 + fib2;
-
-		printf(", %lu", next_fib);
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
 
 		fib1 = fib2;
-		fib2 = next_fib;
+		fib2 = sum;
 	}
 
-	printf("\n");
+	fib1_high1 = fib1 / 10000000000;
+	fib2_high1 = fib2 / 10000000000;
+	fib1_low1 = fib1 % 10000000000;
+	fib2_low1 = fib2 % 10000000000;
 
+	for (count = 93; count < 99; count++)
+	{
+		high1 = fib1_high1 + fib2_high1;
+		low1 = fib1_low1 + fib2_low1;
+		if (fib1_low1 + fib2_low1 > 9999999999)
+		{
+			high1 += 1;
+			low1 %= 10000000000;
+		}
+
+	printf("%lu%lu", high1, low1);
+	if (count != 98)
+		printf(", ");
+
+	fib1_high1 = fib2_high1;
+	fib1_low1 = fib2_low1;
+	fib2_high1 = high1;
+	fib2_low1 = low1;
+	}
+	printf("\n");
 	return (0);
 }
